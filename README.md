eef3578b541492becc21971f4ec528668e0ab46b
pq100 <system32bit@naver.com>
2024-07-30 11:42:05 +0900

    semiprojectv1

---
+++ README.md
@@ -0,0 +1,13 @@
+# 세미 프로젝트 v1
+
++ 메인 🌏
++ 회원 👨‍💻
+  + 회원가입 - /member/join
+  + 로그인 - /member/login
+  + 회원정보 - /member/myinfo
++ 게시판
+  + 게시판 - /board/list
+  + 글쓰기 - /board/write
+  + 본문글 - /board/view
+  + 본문글삭제 - /board/remove
+  + 본문글수정 - /board/modify
---
+++ app/routes/board.py
@@ -0,0 +1,29 @@
+from fastapi import APIRouter, Request
+from starlette.responses import HTMLResponse
+from starlette.templating import Jinja2Templates
+
+# 라우터 생성
+board_router = APIRouter()
+# 템플릿 지정
+templates = Jinja2Templates(directory='views/templates')
+
+
+@board_router.get("/write", response_class=HTMLResponse)
+async def write(req: Request):
+    return templates.TemplateResponse('board/write.html', {'request': req})
+
+@board_router.get("/list", response_class=HTMLResponse)
+async def list(req: Request):
+    return templates.TemplateResponse('board/list.html', {'request': req})
+
+@board_router.get("/view", response_class=HTMLResponse)
+async def view(req: Request):
+    return templates.TemplateResponse('board/view.html', {'request': req})
+
+@board_router.get("/remove", response_class=HTMLResponse)
+async def remove(req: Request):
+    return templates.TemplateResponse('board/remove.html', {'request': req})
+
+@board_router.get("/modify", response_class=HTMLResponse)
+async def modify(req: Request):
+    return templates.TemplateResponse('board/modify.html', {'request': req})
